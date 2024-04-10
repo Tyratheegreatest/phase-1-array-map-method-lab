dd6a39e02b8c1d38d1ee84af966d71db56877cca
tyranorah <tashnorah@gmail.com>
2024-04-10 03:19:36 +0300

    index.js

--- index.js
+++ index.js
@@ -12,5 +12,14 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(sentence =>{
+    const words = sentence.split(' ');
+
+const titleCasedWords = words.map((word) =>{
+  return word.charAt(0).toUpperCase()+word.slice(1)
+})
+  return titleCasedWords.join(' ')
+})
 }
+
+console.log(titleCased());
